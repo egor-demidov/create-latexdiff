095dd85c2ea0343ccf2d0a772270b4431e6b74c7
egor-demidov <157317669+egor-demidov@users.noreply.github.com>
2024-06-10 14:54:44 -0400

    Added shell-escape option

--- make_diff.sh
+++ make_diff.sh
@@ -5,17 +5,17 @@ set -e
 cp /.latexmkrc ~/.latexmkrc
 
 # Compile the current document
-latexmk -pdf -interaction=nonstopmode $1
+latexmk -pdf -interaction=nonstopmode -shell-escape $1
 
 # Compile the base version of the document
 unzip base.zip
 cd base
-latexmk -pdf -interaction=nonstopmode $1
+latexmk -pdf -interaction=nonstopmode -shell-escape $1
 
 # Generate and compile the diff
 cd ..
 latexdiff --flatten base/$1 $1 > diff.tex
-latexmk -pdf -interaction=nonstopmode diff.tex
+latexmk -pdf -interaction=nonstopmode -shell-escape diff.tex
 
 rm -rf base
 rm *.bbl || true
